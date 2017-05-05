f997d35228783bc97b6b3cafd288318a8d6a82b2
Kazys <kazys.stepanas@csiro.au>
2017-05-05 11:47:24 +1000

    Comment fix.

--- cpp/3es-core/3esservermacros.h
+++ cpp/3es-core/3esservermacros.h
@@ -170,7 +170,7 @@
 
 /// Stop the server. The server is closed and disposed and is no longer valid for use after
 /// this call.
-/// No the @p server argument must be a pointer as it is first checked against null, then
+/// Note the @p server argument must be a pointer as it is first checked against null, then
 /// cleared to @c nullptr.
 /// @param server The @c Server or @c Connection object. Must be a dereferenced pointer.
 #define TES_SERVER_STOP(serverPtr) \
