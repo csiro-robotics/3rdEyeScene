de947d46bf66fe9f344358def64711b4886df434
Kazys Stepanas <kazys.stepanas@csiro.au>
2017-05-08 09:56:20 +1000

    Work in progress on log window. First iteration of sizer button for sizing UI panels.

---
+++ 3rdEyeScene/Assets/Scripts/UI/Sizer.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+namespace UI
+{
+	public class Sizer : MonoBehaviour
+	{
+		[SerializeField]
+		private RectTransform _panel;
+		public RectTransform Panel
+		{
+			get { return _panel; }
+			set { _panel = value; }
+		}
+		[SerializeField]
+		private float _minSize;
+		public float MinSize
+		{
+			get { return _minSize; }
+			set { _minSize = value; }
+		}
+
+		/// <summary>
+		/// Update is called every frame, if the MonoBehaviour is enabled.
+		/// </summary>
+		void Update()
+		{
+			if (_dragging && _panel != null)
+			{
+				Vector3 dragPos = CursorPosition;
+				float deltaY = dragPos.y - _lastMousePos.y;
+
+				if (deltaY != 0)
+				{
+					Vector2 panelSize = _panel.sizeDelta;
+					panelSize.y = Mathf.Max(panelSize.y + deltaY, _minSize);
+					_panel.sizeDelta = panelSize;
+				}
+
+				_lastMousePos = dragPos;
+			}
+		}
+
+		public void OnDragStart()
+		{
+			_lastMousePos = CursorPosition;
+			_dragging = true;
+		}
+
+		public void OnDragEnd()
+		{
+			_dragging = false;
+		}
+
+		private Vector3 CursorPosition
+		{
+			get
+			{
+				return Input.mousePosition;
+			}
+		}
+
+		private Vector3 _lastMousePos;
+		private bool _dragging = false;
+	}
+}
